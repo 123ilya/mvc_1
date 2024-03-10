f040715985f1b170f14155bb4a75a43c08ecd5e3
123ilya <123ilya@gmail.com>
2024-03-10 16:34:01 +1000

    48:00

--- core/Router.php
+++ core/Router.php
@@ -5,24 +5,29 @@ namespace app\core;
 class Router
 {
     public Request $request;
-    protected array $routes = [];
+    public Response $response;
 
-    public function __construct(Request $request)
+    protected array $routes = [];
+    //------------------------------------------------------------------------------------------
+
+    public function __construct(Request $request, Response $response)
     {
         $this->request = $request;
+        $this->response = $response;
     }
-
+    //----------------------------------------------------------------------------
     public function get($path, $callback)
     {
         $this->routes['get'][$path] = $callback;
     }
-
+    //------------------------------------------------------------------------------------------------
     public function resolve()
     {
         $path =  $this->request->getPath();
         $method = $this->request->getMethod();
         $callback = $this->routes[$method][$path] ?? \false;
         if ($callback === \false) {
+            $this->response->setStatusCode(404);
             return 'Not found';
         }
         if (\is_string($callback)) {
@@ -31,22 +36,21 @@ class Router
 
         return \call_user_func($callback);
     }
-
+    //-----------------------------------------------------------------------------------------------
     public function renderView($view)
     {
         $layoutContent = $this->layoutContent();
         $viewContent = $this->renderOnlyView($view);
         return \str_replace('{{content}}', $viewContent, $layoutContent);
-        include_once  Application::$ROOT_DIR . '/views/' . $view . '.php';
     }
-
+    //------------------------------------------------------------------------------------------------
     protected function layoutContent()
     {
         \ob_start();
         include_once  Application::$ROOT_DIR . '/views/layouts/' . 'main' . '.php';
         return \ob_get_clean();
     }
-
+    //-----------------------------------------------------------------------------------------------
     protected function renderOnlyView($view)
     {
         \ob_start();
