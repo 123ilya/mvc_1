98a648b3ca687d555b40ccdb0b3997c499c93625
123ilya <123ilya@gmail.com>
2024-03-19 11:00:36 +1000

    24:00

--- core/Model.php
+++ core/Model.php
@@ -11,6 +11,7 @@ abstract class Model
     public const RULE_MAX = 'max';
     public const RULE_MATCH = 'match';
 
+    public array $errors = [];
 
 
     public function loadData($data)
@@ -26,5 +27,37 @@ abstract class Model
 
     public function validate()
     {
+        foreach ($this->rules() as $attribute => $rules) {
+            $value = $this->{$attribute};
+            foreach ($rules as $rule) {
+                $ruleName = $rule;
+                if (!\is_string($ruleName)) {
+                    $ruleName = $rule[0];
+                }
+                if ($ruleName === self::RULE_REQUIRED && !$value) {
+                    $this->addError($attribute, self::RULE_REQUIRED);
+                }
+                if ($ruleName === self::RULE_EMAIL && !\filter_var($value, \FILTER_VALIDATE_EMAIL)) {
+                    $this->addError($attribute, self::RULE_EMAIL);
+                }
+            }
+        }
+        return empty($this->errors);
+    }
+    public function addError(string $attribute, string $rule)
+    {
+        $message = $this->errorMessages()[$rule] ?? '';
+        $this->errors[$attribute][] = $message;
+    }
+
+    public function errorMessages()
+    {
+        return [
+            self::RULE_REQUIRED => 'This field is requered',
+            self::RULE_EMAIL => 'This field must be valid email address',
+            self::RULE_MIN => 'Min length of this field must be {min}',
+            self::RULE_MAX => 'Max length of this field must be {max}',
+            self::RULE_MATCH => 'This field must be the same as {match}',
+        ];
     }
 }
