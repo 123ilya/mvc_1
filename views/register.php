fe0bfd8b643ba01815c76c3733e76acfb6bb4d71
123ilya <123ilya@gmail.com>
2024-03-20 20:03:17 +1000

    46:00

--- views/register.php
+++ views/register.php
@@ -1,13 +1,30 @@
 <h1>Create an account</h1>
+<?php
 
-<form novalidate action="" method="post">
+use app\core\form\Form;
+?>
+
+<?php $form = Form::begin('', 'post') ?>
+
+<?php echo $form->field($model, 'firstname') ?>
+<?php echo $form->field($model, 'lastname') ?>
+<?php echo $form->field($model, 'email') ?>
+<?php echo $form->field($model, 'password') ?>
+<?php echo $form->field($model, 'confirmPassword') ?>
+
+<button type="submit" class="btn btn-primary">Submit</button>
+
+
+<?php Form::end() ?> 
+
+<!-- <form novalidate action="" method="post">
     <div class="row">
         <div class="col">
             <div class="mb-3">
                 <label class="form-label">Firstname</label>
-                <input type="text" name="firstname" class="form-control <?php echo $model->hasError('firstname') ? 'is-invalid' : '' ?>" value="<?php echo $model->firstname ?>">
+                <input type="text" name="firstname" class="form-control <?php //echo $model->hasError('firstname') ? 'is-invalid' : '' ?>" value="<?php //echo $model->firstname ?>">
                 <div class="invalid-feedback">
-                    <?php echo $model->getFirstError('firstname') ?>
+                    <?//php echo $model->getFirstError('firstname') ?>
                 </div>
             </div>
         </div>
@@ -35,4 +52,4 @@
 
 
     <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+</form> -->
